dac42f7b0ccbed6ca06409e6be5babe49a14966a
Static <yashmhaparle2004@gmail.com>
2025-03-27 23:58:48 +0530

    Added Siri wave and Startup sound.

--- www/main.js
+++ www/main.js
@@ -9,5 +9,29 @@ $(document).ready(function () {
         out:{
             effect: "bounceOut",
         },
-    })
+    });
+
+    //Siri configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios",
+        speed: "0.30",
+        autostart: true
+      });
+
+    //Siri Message Animation
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "fadeInUP",
+            sync: true,
+        },
+        out:{
+            effect: "fadeOutUp",
+            sync: true,
+        },
+    });
 });
